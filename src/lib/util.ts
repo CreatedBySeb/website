50c4599caa0975400e4b96fbe24faa30d8de34c5
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2024-11-17 22:08:10 +0000

    Add workaround for C# slug

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,8 @@
 export const dateFormatter = new Intl.DateTimeFormat(undefined, { day: "numeric", month: "long", weekday: "long", year: "numeric" });
 
 export function slugify(string: string): string {
-	return string.toLowerCase().replace(".", "-");
+	return string
+		.toLowerCase()
+		.replace(".", "-")
+		.replace("#", "sharp"); // For C#
 }
