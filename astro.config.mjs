08ce5191af32d7bb5c99e3c5f3ae92f0f9b5880e
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2025-12-20 11:00:38 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.sebastienfulmer.com",
-	integrations: [
-		icon(),
-	],
+	integrations: [icon(), sitemap()],
 });
