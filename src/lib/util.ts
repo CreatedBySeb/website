88a2445aa80ff188505b2ca98642cffd1b224aeb
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2025-12-22 23:16:48 +0000

    Add content data to meta/opengraph

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,3 +6,7 @@ export function slugify(string: string): string {
 		.replace(".", "-")
 		.replace("#", "sharp"); // For C#
 }
+
+export function toISODate(date: Date): string {
+	return date.toISOString().split("T", 1).shift() as string;
+}
